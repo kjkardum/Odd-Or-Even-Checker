3b33854b92436c64b15e97cc16646232bc7c7108
kjkardum <62484925+kjkardum@users.noreply.github.com>
2021-05-24 22:31:20 +0200

    Increased supported numbers from 3981 to 3982.
    My friend tried to use this project but the program returned inconclusive result for number he wanted to try: 3982, so by making minimal changes to codebase, following it's current code layout, I've made a change that allows to conclusively answer for number 3982. Of course this has been thoroughly tested and verified using state of the art unit tests.

--- OddOrEven.py
+++ OddOrEven.py
@@ -11942,8 +11942,11 @@ def odd_or_even(number: int):
     if number == 3980:
         return "Even"
     
-    if number == 3981
+    if number == 3981:
         return "Odd"
+    
+    if number == 3982:
+        return "Even"
 
 if __name__ == "__main__":
     while True:
